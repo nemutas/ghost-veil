725fba256418f9ebce7a7b1e497e90ebabd0bde8
nemutas <inohiro.focru@gmail.com>
2022-07-03 03:07:08 +0900

    adjust veil speed

--- src/home/shader/planeFrag.glsl
+++ src/home/shader/planeFrag.glsl
@@ -9,7 +9,8 @@ void main() {
   float gray = (tex.r + tex.g + tex.b) / 3.0;
   gray *= 0.5;
 
-  float speed = (cnoise21(v_uv * 10.0) + 1.0) * 0.5;
+  float speed = (cnoise21(v_uv * 10.0) + 1.0) * 0.5; // 0 - 1
+  speed = speed * (1.0 - 0.2) + 0.2; // 0.2 - 1
   float edge = pow(1.0 - u_progress, speed);
   float a = step(edge, v_uv.y);
 
