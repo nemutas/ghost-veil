baba9039b0e26071c9ead9c090b14922422d0db2
nemutas <inohiro.focru@gmail.com>
2022-07-03 03:06:09 +0900

    Updates

--- assets/home.351879e4.js
+++ assets/home.351879e4.js
@@ -3772,7 +3772,8 @@ void main() {\r
   float gray = (tex.r + tex.g + tex.b) / 3.0;\r
   gray *= 0.5;
 
-  float speed = (cnoise21(v_uv * 10.0) + 1.0) * 0.5;\r
+  float speed = (cnoise21(v_uv * 10.0) + 1.0) * 0.5; 
+  speed = speed * (1.0 - 0.2) + 0.2; 
   float edge = pow(1.0 - u_progress, speed);\r
   float a = step(edge, v_uv.y);
 
